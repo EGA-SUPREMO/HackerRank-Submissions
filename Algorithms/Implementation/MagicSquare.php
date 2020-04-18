70c55c4bec70d60bdfebe610565d0d27ed27be28
Ale <cxiesestro@gmail.com>
2020-04-18 18:10:02 -0400

    Update MagicSquare.php
    this works

--- Algorithms/Implementation/MagicSquare.php
+++ Algorithms/Implementation/MagicSquare.php
@@ -1,97 +1,60 @@
-?php
+<?php
 
-const MagicSquareConstant = 15;
+const MAGIC_SQUARE_CONSTANT = 15;
+const MAGIC_SQUARES = [[[8, 1, 6],
+                        [3, 5, 7],
+                        [4, 9, 2]],
+
+                       [[6, 1, 8],
+                        [7, 5, 3],
+                        [2, 9, 4]],
+
+                       [[2, 7, 6],
+                        [9, 5, 1],
+                        [4, 3, 8]],
+
+                       [[4, 3, 8],
+                        [9, 5, 1],
+                        [2, 7, 6]],
+
+                       [[2, 9, 4],
+                        [7, 5, 3],
+                        [6, 1, 8]],
+
+                       [[4, 9, 2],
+                        [3, 5, 7],
+                        [8, 1, 6]],
+
+                       [[6, 7, 2],
+                        [1, 5, 9],
+                        [8, 3, 4]],
+
+                       [[8, 3, 4],
+                        [1, 5, 9],
+                        [6, 7, 2]]];
 
 // Complete the formingMagicSquare function below.
 function formingMagicSquare($array)
 {
-    $totalSums = totalSums($array);
-    $diagonalTotalSums = diagonalTotalSums($array);
-    $differences = differences($totalSums) + diagonalDifferences($diagonalTotalSums);
+    $absoluteCosts = array_fill(0, count(MAGIC_SQUARES), 0);
+    $minimumCost = 0;
 
-
-
-    var_dump($totalSums);
-    var_dump($diagonalTotalSums);
-    var_dump($differences);
-}
-
-function totalSums($array)
-{
     $length = count($array[0]);
-    $totalSums = ['row' => array_fill(0, $length, 0),
-                 'column' => array_fill(0, $length, 0)];
-
-    for($x = 0; $x < $length; $x++) {
-        for($y = 0; $y < $length; $y++) {
-            $totalSums['row'][$x] += $array[$x][$y];
+    for ($i=0; $i < count(MAGIC_SQUARES); $i++) {
+        for ($x = 0; $x < $length; $x++) {
+            for ($y = 0; $y < $length; $y++) {
+                $difference = $array[$x][$y] - MAGIC_SQUARES[$i][$x][$y];
+                $absoluteCosts[$i] += abs($difference);
+            }
         }
     }
-    for($x = 0; $x < $length; $x++) {
-        for($y = 0; $y < $length; $y++) {
-            $totalSums['column'][$x] += $array[$y][$x];
+    for ($i=0; $i < count(MAGIC_SQUARES); $i++) {
+        if ($absoluteCosts[$i] < $minimumCost) {
+            $minimumCost = $absoluteCosts[$i];
         }
     }
-
-    return $totalSums;
-}
-
-function differences($totalSums)
-{
-    $length = count($totalSums['row']);
-    $differences = ['row' => [],
-                    'column' => []];
-
-    for($i = 0; $i < $length; $i++) {
-        $differences['row'][] = $totalSums['row'][$i] - MagicSquareConstant;
-    }
-    for($i = 0; $i < $length; $i++) {
-        $differences['column'][] = $totalSums['column'][$i] - MagicSquareConstant;
-    }
-
-    return $differences;
-}
-
-function diagonalTotalSums($array)
-{
-    $length = count($array);
-    $totalSums = [0, 0];
-
-    for($i = 0; $i < $length; $i++) {
-        $totalSums[0] += $array[$i][$i];
-    }
-    for($i = 0; $i < $length; $i++) {
-        $totalSums[1] += $array[($length-1) - $i][$i];
-    }
-
-    return $totalSums;
-}
-
-function diagonalDifferences($diagonalTotalSums)
-{
-    $differences = ['diagonal' => []];
-
-    for($i = 0; $i < count($diagonalTotalSums); $i++) {
-        $differences['diagonal'][] = $diagonalTotalSums[$i] - MagicSquareConstant;
-    }
-
-    return $differences;
-}
-
-///////////---
-
-function possibilities($differences)
-{
-$array = ['cases' => ]
-}
-
-class Possibility
-{
-    $possibilities = ['' ]
-    function __construct(argument)
-    {
-        # code...
-    }
+    
+    return $minimumCost;
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
